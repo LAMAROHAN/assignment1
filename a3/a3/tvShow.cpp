a2ba97bd4e0c47a98d5b6a4844ec02b029026ff5
Rohan Shyntan Tamang <rohan19lama19@gmail.com>
2026-03-28 15:51:31 -0400

    Update tvShow.cpp

--- a3/a3/tvShow.cpp
+++ a3/a3/tvShow.cpp
@@ -14,83 +14,99 @@ using namespace std;
 
 namespace seneca {
 
-    TvShow::TvShow(const string& id, const string& title,
-                   const string& summary, unsigned short year)
-        : MediaItem(title, summary, year) {
-        m_id = id;
-    }
-
     TvShow* TvShow::createItem(const string& str) {
-
-        if (str.size() == 0 || str[0] == '#')
+        if (str.empty() || str[0] == '#')
             throw "Not a valid show.";
 
         stringstream ss(str);
 
-        string id, title, year, summary;
+        string id;
+        string title;
+        string yearStr;
+        string summary;
 
         getline(ss, id, ',');
         getline(ss, title, ',');
-        getline(ss, year, ',');
+        getline(ss, yearStr, ',');
         getline(ss, summary);
 
         MediaItem::trim(id);
         MediaItem::trim(title);
-        MediaItem::trim(year);
+        MediaItem::trim(yearStr);
         MediaItem::trim(summary);
 
-        return new TvShow(id, title, summary, stoi(year));
+        return new TvShow(id, title, summary, (unsigned short)stoi(yearStr));
     }
 
     void TvShow::addEpisode(Collection& col, const string& str) {
+        if (str.empty() || str[0] == '#')
+            throw "Not a valid episode.";
 
-        stringstream ss(str);
+        try {
+            stringstream ss(str);
 
-        string showID, season, number, airDate, length, title, summary;
+            string showID;
+            string overallStr;
+            string seasonStr;
+            string numberStr;
+            string airDate;
+            string lengthStr;
+            string title;
+            string summary;
 
-        getline(ss, showID, ',');
-        getline(ss, season, ',');
-        getline(ss, number, ',');
-        getline(ss, airDate, ',');
-        getline(ss, length, ',');
-        getline(ss, title, ',');
-        getline(ss, summary);
+            getline(ss, showID, ',');
+            getline(ss, overallStr, ',');
+            getline(ss, seasonStr, ',');
+            getline(ss, numberStr, ',');
+            getline(ss, airDate, ',');
+            getline(ss, lengthStr, ',');
+            getline(ss, title, ',');
+            getline(ss, summary);
 
-        MediaItem::trim(showID);
-        MediaItem::trim(season);
-        MediaItem::trim(number);
-        MediaItem::trim(airDate);
-        MediaItem::trim(length);
-        MediaItem::trim(title);
-        MediaItem::trim(summary);
+            MediaItem::trim(showID);
+            MediaItem::trim(overallStr);
+            MediaItem::trim(seasonStr);
+            MediaItem::trim(numberStr);
+            MediaItem::trim(airDate);
+            MediaItem::trim(lengthStr);
+            MediaItem::trim(title);
+            MediaItem::trim(summary);
 
-        for (size_t i = 0; i < col.size(); i++) {
+            if (showID.empty() || overallStr.empty() || numberStr.empty() || lengthStr.empty())
+                throw "Not a valid episode.";
 
-            TvShow* show = dynamic_cast<TvShow*>(col[i]);
+            Episode ep;
+            ep.m_season = seasonStr.empty() ? 1 : (unsigned short)stoi(seasonStr);
+            ep.m_numberInSeason = (unsigned short)stoi(numberStr);
+            ep.m_airDate = airDate;
+            ep.m_length = (unsigned int)stoi(lengthStr);
+            ep.m_title = title;
+            ep.m_summary = summary;
 
-            if (show && show->m_id == showID) {
+            bool added = false;
 
-                Episode ep;
-                ep.m_showID = showID;
-                ep.m_season = stoi(season);
-                ep.m_numberInSeason = stoi(number);
-                ep.m_airDate = airDate;
-                ep.m_length = stoi(length);
-                ep.m_title = title;
-                ep.m_summary = summary;
-
-                show->m_episodes.push_back(ep);
+            for (size_t i = 0; i < col.size(); i++) {
+                TvShow* show = dynamic_cast<TvShow*>(col[i]);
+                if (show != nullptr && show->m_id == showID) {
+                    show->m_episodes.push_back(ep);
+                    added = true;
+                    break;
+                }
             }
+
+            if (!added)
+                throw "Not a valid episode.";
+        }
+        catch (...) {
+            throw "Not a valid episode.";
         }
     }
 
     double TvShow::getEpisodeAverageLength() const {
-
-        if (m_episodes.size() == 0)
+        if (m_episodes.empty())
             return 0;
 
         double total = 0;
-
         for (size_t i = 0; i < m_episodes.size(); i++) {
             total += m_episodes[i].m_length;
         }
@@ -99,14 +115,10 @@ namespace seneca {
     }
 
     list<string> TvShow::getLongEpisodes() const {
-
         list<string> result;
 
-        double avg = getEpisodeAverageLength();
-
         for (size_t i = 0; i < m_episodes.size(); i++) {
-
-            if (m_episodes[i].m_length > avg) {
+            if (m_episodes[i].m_length >= 3600) {
                 result.push_back(m_episodes[i].m_title);
             }
         }
@@ -115,9 +127,7 @@ namespace seneca {
     }
 
     void TvShow::display(ostream& out) const {
-
         if (g_settings.m_tableView) {
-
             out << "S | ";
             out << left << setfill('.');
             out << setw(50) << getTitle() << " | ";
@@ -127,7 +137,6 @@ namespace seneca {
             out << left;
 
             if (g_settings.m_maxSummaryWidth > -1) {
-
                 if ((short)getSummary().size() <= g_settings.m_maxSummaryWidth)
                     out << getSummary();
                 else
@@ -140,11 +149,9 @@ namespace seneca {
             out << endl;
         }
         else {
-
             size_t pos = 0;
 
             out << getTitle() << " [" << getYear() << "]\n";
-
             out << setw(getTitle().size() + 7) << setfill('-') << "" << '\n';
 
             while (pos < getSummary().size()) {
@@ -153,22 +160,27 @@ namespace seneca {
             }
 
             for (size_t i = 0; i < m_episodes.size(); i++) {
-
                 out << "    S";
 
                 if (m_episodes[i].m_season < 10)
                     out << "0";
-
                 out << m_episodes[i].m_season;
 
                 out << "E";
 
                 if (m_episodes[i].m_numberInSeason < 10)
                     out << "0";
-
                 out << m_episodes[i].m_numberInSeason;
 
                 out << " " << m_episodes[i].m_title << '\n';
+
+                size_t p = 0;
+                while (p < m_episodes[i].m_summary.size()) {
+                    out << "            "
+                        << m_episodes[i].m_summary.substr(p, g_settings.m_maxSummaryWidth - 8)
+                        << '\n';
+                    p += g_settings.m_maxSummaryWidth - 8;
+                }
             }
 
             out << setw(getTitle().size() + 7) << setfill('-') << "" << setfill(' ') << '\n';
