f42b5ae9cc55ab9e1dc71bb2ad1ed585c9019825
Rohan Shyntan Tamang <rohan19lama19@gmail.com>
2026-03-29 14:49:30 -0400

    Update tvShow.h

--- a3/a3/tvShow.h
+++ a3/a3/tvShow.h
@@ -7,28 +7,27 @@
 #ifndef SENECA_TVSHOW_H
 #define SENECA_TVSHOW_H
 
-#include "mediaItem.h"
 #include <string>
 #include <vector>
 #include <list>
+#include "mediaItem.h"
 
 namespace seneca {
 
-    class Collection;
+    struct TvEpisode {
+        const class TvShow* m_show{};
+        unsigned short m_numberOverall{};
+        unsigned short m_season{};
+        unsigned short m_numberInSeason{};
+        std::string m_airDate{};
+        unsigned int m_length{};
+        std::string m_title{};
+        std::string m_summary{};
+    };
 
     class TvShow : public MediaItem {
-
-        struct Episode {
-            std::string m_title{};
-            unsigned short m_season{};
-            unsigned short m_numberInSeason{};
-            std::string m_airDate{};
-            unsigned int m_length{};
-            std::string m_summary{};
-        };
-
         std::string m_id{};
-        std::vector<Episode> m_episodes{};
+        std::vector<TvEpisode> m_episodes{};
 
     public:
         TvShow(const std::string& id, const std::string& title,
@@ -36,13 +35,13 @@ namespace seneca {
 
         static TvShow* createItem(const std::string& str);
 
-        static void addEpisode(Collection& col, const std::string& str);
+        void display(std::ostream& out = std::cout) const override;
+
+        template<typename Collection_t>
+        static void addEpisode(Collection_t& col, const std::string& str);
 
         double getEpisodeAverageLength() const;
-
         std::list<std::string> getLongEpisodes() const;
-
-        void display(std::ostream& out) const override;
     };
 
 }
